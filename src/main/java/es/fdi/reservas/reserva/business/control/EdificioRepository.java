9bd46f249dd05e006e0159df60b2ba14d2544bc1
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2016-03-04 18:00:40 +0100

    Actualizados los repositorios a JpaRepository
    JpaRepository proporciona métodos específicos para los repositorios que
    proporcionan funcionalidad extra específica de JPA y que CrudRepository
    no lo hace.

--- src/main/java/es/fdi/reservas/reserva/business/control/EdificioRepository.java
+++ src/main/java/es/fdi/reservas/reserva/business/control/EdificioRepository.java
@@ -1,10 +1,10 @@
 package es.fdi.reservas.reserva.business.control;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import es.fdi.reservas.reserva.business.entity.Edificio;
 
 @Repository
-public interface EdificioRepository extends CrudRepository<Edificio, Long>{
+public interface EdificioRepository extends JpaRepository<Edificio, Long>{
 
 }
--- src/main/java/es/fdi/reservas/reserva/business/control/ReservaRepository.java
+++ src/main/java/es/fdi/reservas/reserva/business/control/ReservaRepository.java
@@ -2,13 +2,13 @@ package es.fdi.reservas.reserva.business.control;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import es.fdi.reservas.reserva.business.entity.Reserva;
 
 
 @Repository
-public interface ReservaRepository extends CrudRepository<Reserva, Long>{
+public interface ReservaRepository extends JpaRepository<Reserva, Long>{
 
 	public List<Reserva> findByUsername(String username); 
 	
